7dd6d7ecba52a26405d390dbd080665bb6ba6f1c
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-27 01:52:27 +0200

    Update main.h

--- main.h
+++ main.h
@@ -6,5 +6,6 @@ int _printf(const char *format, ...);
 void _print_rev_recursion(char *s);
 int convert_number(int n, int counter);
 int printfbin(unsigned int b);
+int switching_function(const char *format, int printed_chars, va_list arg);
 
 #endif /* _MAIN_H_ */
