e105bc8312d888ec6ae5118adf6e2217dae9812b
naitelkadir <111744649+naitelkadir@users.noreply.github.com>
2023-03-27 01:10:53 +0200

    update convert_number.c

--- convert_number.c
+++ convert_number.c
@@ -10,39 +10,30 @@
 *
 * Return: counter
 */
-int convert_number(int n)
+int convert_number(int n, int counter)
 {
-int num, mod = n % 10, digit, m = 1;
-int c = 1;
+int d;
+int r;
 
-n = n / 10;
-num = n;
 if (n < 0)
 {
-_putchar('-');
-num = -num;
-n = -n;
-mod = -mod;
-c++;
+_putchar(45);
+counter = counter + 1;
 }
-if (num > 0)
-{
-while (num / 10 != 0)
-{
-m = m * 10;
-num = num / 10;
-}
-num = n;
-while (m > 0)
-{
-digit = num / m;
-_putchar(digit + '0');
-num = num - (digit * m);
-m = m / 10;
-c++;
-}
-}
-_putchar(mod + '0');
-return (c);
+n = abs(n);
 
+d = n / 10;
+r = n % 10;
+if (n < 10)
+{
+_putchar(n + 48);
+counter = counter + 1;
+}
+else
+{
+counter = counter + 1;
+counter += convert_number(d, counter);
+_putchar(r + 48);
+}
+return (counter);
 }
