8f63b457e8ebc9f18b9e7b5288f606c0d966c4b6
naitelkadir <111744649+naitelkadir@users.noreply.github.com>
2023-03-26 23:33:19 +0200

    update main.h

--- main.h
+++ main.h
@@ -5,5 +5,6 @@ int _putchar(char c);
 int _printf(const char *format, ...);
 void _print_rev_recursion(char *s);
 int convert_number(int n, int counter);
+int printfbin(unsigned int b);
 
 #endif /* _MAIN_H_ */
