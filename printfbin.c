49b89ea9d7de5eb095d2f92262a602048f2baadc
naitelkadir <111744649+naitelkadir@users.noreply.github.com>
2023-03-27 09:56:47 +0000

    update printfbin.c

--- printfbin.c
+++ printfbin.c
@@ -4,34 +4,28 @@
 /**
  * printfbin - convert int to binary.
  * @b: number to convert.
- * Return: number of printed caracters.
+ * Return: void
  */
 
-int printfbin(unsigned int b)
+void printfbin(unsigned int b)
 {
-	int flag = 0;
-	int cont = 0;
-	int i, a = 1, n;
-	unsigned int p;
-
-	for (i = 0; i < 32; i++)
+	int i=0;
+	int binaryNum[32];
+	
+	if (b == 0)
 	{
-		p = ((a << (31 - i)) & b);
-		if (p >> (31 - i))
-			flag = 1;
-		if (flag)
-		{
-			n = p >> (31 - i);
-			putchar(n + 48);
-			cont++;
-		}
-	}
-	if (cont == 0)
+      	putchar('0');
+      	return;
+    	}
+   	while (b > 0)
 	{
-		cont++;
-		putchar('0');
+      		binaryNum[i++] = b % 2;
+      		b /= 2;
+   	}
+   	for (int j = i-1; j >= 0; j--)
+	{
+      		putchar('0' + binaryNum[j]);
 	}
-	return (cont);
 }
 
 
