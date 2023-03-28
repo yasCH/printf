709829de2ab16d04510834ebea0063ea5cdac177
yasCH <43391964+yasCH@users.noreply.github.com>
2023-03-29 01:06:03 +0200

    Update printfbin.c

--- printfbin.c
+++ printfbin.c
@@ -2,21 +2,19 @@
 #include "main.h"
 
 /**
- * printfbin - convert int to binary.
- * @b: number to convert.
- * Return: number of printed caracters
- */
-
+* printfbin - convert int to binary.
+* @b: number to convert.
+* Return: number of printed caracters
+*/
 int printfbin(unsigned int b)
 {
-    if (b == 0) 
-    {
-        return (0);
-    }
-   if (b == 1)
-    {
-        return (1);   
-    }
-     return (b % 2) + 10 * printfbin(b / 2);
-
+if (b == 0)
+{
+return (0);
+}
+if (b == 1)
+{
+return (1);
+}
+return ((b % 2) + 10 * printfbin(b / 2));
 }
